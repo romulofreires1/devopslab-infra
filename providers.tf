79841e55e56e9e50be3a6f15caeb7a34418b4777
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2022-09-08 19:13:33 -0300

    removinf credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/gcpkey.json")
-
   project = "lab-devops-cloud-56845"
   region  = "us-west1"
   zone    = "us-west1-b"
