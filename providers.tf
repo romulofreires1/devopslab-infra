908b99ff7f634badc613cf831728a6df4ede3aa7
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2022-09-08 18:53:17 -0300

    nome do projeto

--- providers.tf
+++ providers.tf
@@ -10,9 +10,9 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpkey.json")
+  credentials = file("/home/gcpkey.json")
 
-  project = "teste-351601"
+  project = "lab-devops-cloud-56845"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
