b8fd77b60e212f9034420d906a480e1c4c5307e0
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2022-09-11 09:49:25 -0300

    updating registry

--- gcp-main.tf
+++ gcp-main.tf
@@ -3,9 +3,9 @@ resource "google_app_engine_application" "app" {
   location_id = "us-central"
 }
 
-resource "google_artifact_registry_repository" "https://github.com/romulofreires1/devopslab" {
+resource "google_artifact_registry_repository" "my-repo" {
   location = "us-central1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
-}
+}s
